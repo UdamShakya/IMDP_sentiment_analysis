bf83928500e4668f5c61519cddf43c72c0b27889
UdamShakya <udamilukpotha@gmail.com>
2025-09-28 23:34:43 +0530

    Loaded the imdb data set from tensorflow

---
+++ src/data_loader.py
@@ -0,0 +1,10 @@
+# src/data_loader.py
+from tensorflow.keras.datasets import imdb
+
+def load_data(num_words=10000):
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=num_words)
+    print(f"Training samples: {len(x_train)}, Test samples: {len(x_test)}")
+    return (x_train, y_train), (x_test, y_test)
+
+if __name__ == "__main__":
+    load_data()

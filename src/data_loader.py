b52be06e9946bdac9edf892a9543bd5a2f829c39
UdamShakya <udamilukpotha@gmail.com>
2025-09-28 23:44:53 +0530

    ignoreed the ssl error

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,5 @@
 # src/data_loader.py
-from tensorflow.keras.datasets import imdb
+from tensorflow.keras.datasets import imdb # type: ignore
 
 def load_data(num_words=10000):
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=num_words)
